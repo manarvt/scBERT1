09b27738964dd70dd94440867492f7a9d3a69bea
manarvt <114369090+manarvt@users.noreply.github.com>
2024-09-20 10:34:09 -0400

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -29,7 +29,7 @@ from utils import *
 import pickle as pkl
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--local_rank", type=int, default=-1, help='Local process rank.')
+parser.add_argument("--local_rank", type=int, default=0, help='Local process rank.')
 parser.add_argument("--bin_num", type=int, default=5, help='Number of bins.')
 parser.add_argument("--gene_num", type=int, default=16906, help='Number of genes.')
 parser.add_argument("--epoch", type=int, default=100, help='Number of epochs.')
